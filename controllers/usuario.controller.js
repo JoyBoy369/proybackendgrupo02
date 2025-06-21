bc025a35c13ee4e825edf8764898c785cd2d6ee4
FloresRodrigo <rodrigoef25@gmail.com>
2025-06-21 17:51:11 -0300

    Flores Rodrigo - bknd creado de usuario
    Backend de usuario con el modelo, controller, rutas, y actualizacion de index, database y packages

---
+++ controllers/usuario.controller.js
@@ -0,0 +1,65 @@
+const Usuario = require('../models/usuario');
+const usuarioCtrl = {}
+
+usuarioCtrl.createUsuario = async (req, res) => {
+    var usuario = new Usuario(req.body);
+    try {
+        await usuario.save();
+        res.json({
+            'status': '1',
+            'msg': 'Usuario guardado.'
+        })
+    } catch (error) {
+        res.status(400).json({
+            'status': '0',
+            'msg': 'Error procesando operacion.'
+        })
+    }
+}
+
+usuarioCtrl.getUsuarios = async (req, res) => {
+    var usuarios = await Usuario.find();
+    res.json(usuarios);
+}
+
+usuarioCtrl.getUsuariosByRol = async (req, res) => {
+    var usuarios = await Usuario.find({ rol: req.params.rol });
+    res.json(usuarios);
+}
+
+usuarioCtrl.getUsuario = async (req, res) => {
+    const usuario = await Usuario.findById(req.params.id);
+    res.json(usuario);
+}
+
+usuarioCtrl.editUsuario = async (req, res) => {
+    try {
+        await Usuario.updateOne({ _id: req.params.id }, req.body);
+        res.json({
+            'status': '1',
+            'msg': 'Usuario actualizado.'
+        })
+    } catch (error) {
+        res.status(400).json({
+            'status': '0',
+            'msg': 'Error procesando la operacion.'
+        })
+    }
+}
+
+usuarioCtrl.deleteUsuario = async (req, res) => {
+    try {
+        await Usuario.deleteOne({ _id: req.params.id });
+        res.json({
+            status: '1',
+            msg: 'Usuario removido.'
+        })
+    } catch (error) {
+        res.status(400).json({
+            'status': '0',
+            'msg': 'Error procesando la operacion.'
+        })
+    }
+}
+
+module.exports = usuarioCtrl;
---
+++ routes/usuario.route.js
@@ -0,0 +1,14 @@
+const usuarioCtrl = require('../controllers/usuario.controller');
+
+const express = require('express');
+const usuario = require('../models/usuario');
+const router = express.Router();
+
+router.post('/', usuarioCtrl.createUsuario);
+router.get('/', usuarioCtrl.getUsuarios);
+router.get('/rol/:rol', usuarioCtrl.getUsuariosByRol);
+router.get('/:id', usuarioCtrl.getUsuario);
+router.put('/:id', usuarioCtrl.editUsuario);
+router.delete('/:id', usuarioCtrl.deleteUsuario);
+
+module.exports = router;
