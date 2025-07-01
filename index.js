691db44a3c8a23cd828401e5af1476107ff8cd63
beto_lage16@hotmail.com <beto_lage16@hotmail.com>
2025-07-01 15:18:26 -0300

    CRUD reserva completo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors({ origin: 'http://localhost:4200' }));
 app.use('/api/usuario', require('./routes/usuario.route.js'));
 app.use('/api/pelicula', require('./routes/pelicula.route.js'));
 app.use('/api/funcion', require('./routes/funcion.route.js'));
-
+app.use('/api/reserva', require('./routes/reserva.route.js'));
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), () => {
--- models/reserva.js
+++ models/reserva.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const Pelicula = require('./pelicula');
-const funcion = require('./funcion');
-const usuario = require('./usuario');
+const Funcion = require('./funcion')
 
 const ReservaSchema = new Schema({
-    funcion: { type: Schema.Types.ObjectId, ref: funcion, required: true },
-    precio: { type: Number, required: true },
-    fecha: { type: Date, default: Date.now, required: true },
-    hora: { type: String, required: true },
-    usuario: { type: Schema.Types.ObjectId, ref: usuario, required: true },
-    pagado: { type: Boolean, default: false, required: true },
-}, {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-    id: false
-});
-module.exports = mongoose.models.Reserva || mongoose.model('Reserva', ReservaSchema);
+    usuario: {type: String, required: false},
+    funcion: { type: Schema.Types.ObjectId, ref: Funcion, required: true },
+    cantidadReservas: { type: Number, required: true},
+    fecha: { type: Date, required: true },
+    precioFinal: { type: Number, required: true },
+    butacasReservadas: { type: [String], default: [] },
+    qr: { type: String, required: false}
+})
+module.exports = mongoose.models.Reserva || mongoose.model('Reserva', ReservaSchema); 
