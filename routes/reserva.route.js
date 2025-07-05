83626f6fa3b0f35fdc2afa693bd21c446804e661
FloresRodrigo <rodrigoef25@gmail.com>
2025-07-05 16:53:10 -0300

    Flores Rodrigo - Reserva rutas
    Se agrego rutas adicionales para reserva

--- routes/reserva.route.js
+++ routes/reserva.route.js
@@ -2,6 +2,13 @@ const reservaCtrl = require('./../controllers/reserva.controller');
 const express = require('express');
 const router = express.Router();
 
+router.get('/buscar/:id', reservaCtrl.getReservasByUser);
+router.get('/resumensemanal', reservaCtrl.getResumenSemanal);
+router.get('/ingresossemanal', reservaCtrl.getIngresosSemanales);
+router.get('/ingresosanuales', reservaCtrl.getIngresosAnuales);
+router.get('/asistenciafuncion', reservaCtrl.getAsistenciaPorFuncion);
+router.get('/ventaspelicula', reservaCtrl.getVentasPorPelicula);
+router.get('/ventasultimomes', reservaCtrl.getTotalVentasUltimoMes);
 router.get('/', reservaCtrl.getReservas);
 router.post('/', reservaCtrl.createReserva);
 router.put('/:id', reservaCtrl.editReserva);
